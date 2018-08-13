f2a7497d0ba7e15c45ae2b0d1deab9963089a2a3
Pallavi Nawani <37789944+Pall104@users.noreply.github.com>
2018-08-13 12:33:11 +0530

    person component made

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
-    return (
+   return (
       <div className="App">
       <h1>Hi!I am a react app</h1>
+      <Person name="Pallavi" age="24"/>
       </div>
-    );
+   );
+  // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hi I\'m a React App!!!'));
   }
 }
 
