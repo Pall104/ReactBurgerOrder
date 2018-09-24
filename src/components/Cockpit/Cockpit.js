52d9433dfb3d322805f6550be00dbc8838b34221
Pallavi Nawani <nawani.pall@gmail.com>
2018-09-24 17:37:04 +0530

    high order component

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import classes from './Cockpit.css'
+import Aux from '../../hoc/Aux';
+import classes from './Cockpit.css';
 const cockpit = (props) => {
 
     let assignedClasses = [];
-    let btnClass = '';
+    let btnClass = classes.Button;
     if(props.showPersons){
-        btnClass = classes.Red;
+        btnClass = [classes.Button,classes.Red].join(' ');
     }
     if(props.persons.length <=2){
       assignedClasses.push(classes.red);
@@ -14,10 +15,12 @@ const cockpit = (props) => {
       assignedClasses.push(classes.bold);
     }
      return (
+       <Aux>
          <div className={classes.Cockpit}>
         <h1 className={assignedClasses.join( ' ' )} >Welcome to the React App!!</h1>
         <button className={btnClass} onClick={props.clicked}>Toggle Persons</button>
         </div>
+        </Aux>
      );
 };
 export default cockpit;
